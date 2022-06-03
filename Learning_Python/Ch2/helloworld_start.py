1bf3a6289bed7f4ed256741715a98887d206e751
pscally1005 <mets1005@yahoo.com>
2022-06-03 09:32:02 -0400

    first commit

---
+++ Learning_Python/Ch2/helloworld_start.py
@@ -0,0 +1,9 @@
+#
+# Example file for HelloWorld
+# LinkedIn Learning Python course by Joe Marini
+#
+
+def main():
+    print("Hello World!")
+    name = input("What is your name? ")
+    print("Nice to meet you ", name)
